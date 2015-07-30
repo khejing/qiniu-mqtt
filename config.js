b73a4e72f9eea128feaba7f09a80c9039447fcd4
khejing <khejing3@aliyun.com>
2015-07-30 18:03:51 +0800

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'ACCESS_KEY': '<Your Access Key>',
+    'SECRET_KEY': '<Your Secret Key>',
+    'Bucket_Name': '<Your Bucket Name>'
+};
---
+++ index.js
@@ -0,0 +1,44 @@
+var mqtt = require('mqtt');
+var qiniu = require('qiniu');
+var config = require('./config.js');
+
+qiniu.conf.ACCESS_KEY = config.ACCESS_KEY;
+qiniu.conf.SECRET_KEY = config.SECRET_KEY;
+
+var uptoken = new qiniu.rs.PutPolicy2({
+	scope: config.Bucket_Name,
+	returnBody: {
+		"success": true
+	}
+});
+
+var topic = "qiniu";
+var mqttClientInstance = mqtt.connect("mqtt://localhost:1883", {clientId: topic});
+mqttClientInstance.on('connect', function () {
+    console.log("connect mqtt server success");
+});
+mqttClientInstance.on('error',function(error) {
+    console.log("mqtt connect failed: ", error);
+});
+mqttClientInstance.subscribe(topic);
+
+mqttClientInstance.on('message', function(messageTopic, data) {
+	console.log("recv msg: "+data);
+	var msg = JSON.parse(data);
+	if(msg.request === "uptoken"){
+		mqttClientInstance.publish(msg.clientId, JSON.stringify({uptoken: uptoken.token()}));
+	} else if(msg.request === "downtoken"){
+		var key = msg.key,
+		    domain = msg.domain;//domain must not contain http:// or https://, and must not contain the last "/" too
+        var baseUrl = qiniu.rs.makeBaseUrl(domain, key);
+        var policy = new qiniu.rs.GetPolicy();
+        mqttClientInstance.publish(msg.clientId, JSON.stringify({url: policy.makeRequest(baseUrl)}));
+	} else {
+		console.log("unknown msg");
+	}	
+});
+/* when got exit signal, then exit
+mqttClientInstance.end();
+mqttClientInstance = null;
+console.log("mqtt client has been destroyed");
+*/

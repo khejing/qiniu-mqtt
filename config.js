b3cc4df1adceb9f3f1491c739a04cd2bbc41e3bd
khejing <khejing3@aliyun.com>
2015-07-31 17:39:45 +0800

    位置标错了

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'ACCESS_KEY': '<Your Access Key>',
     'SECRET_KEY': '<Your Secret Key>',
-    'Domain': '<Your Domain>',
-    'Bucket_Name': '<Your Bucket Name>'//domain must not contain http:// or https://, and must not contain the last "/" too
+    'Domain': '<Your Domain>',//domain must not contain http:// or https://, and must not contain the last "/" too
+    'Bucket_Name': '<Your Bucket Name>'
 };
